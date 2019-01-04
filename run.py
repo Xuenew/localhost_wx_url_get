03f5b57317b0ba63bf299db59df8554af3e6d446
Xuenew <18188108851@163.com>
2019-01-04 09:29:14 +0800

    2019.1.4morning do

--- run.py
+++ run.py
@@ -9,10 +9,21 @@ def hello_world():
     return 'Hello World!'
 @app.route("/search_music/<name>",methods=['GET'])
 def back_song_list(name):
+    """
+    返回 输入音乐名字的搜索结果 默认 0-19 默认 播放链接是第一个
+    :param name:
+    :return:
+    """
     name=name
     p=get_music_all_url(music_name=name)
     return p.get_music_song()
-
-
+@app.route("/index_hot_image",methods=['GET'])
+def back_index_hot_image():
+    """
+    返回 hot image 滑动的图片list
+    :return:
+    """
+    p=get_music_all_url()
+    return p.get_hot_image()
 if __name__ == '__main__':
     app.run()
--- spider_music_get.py
+++ spider_music_get.py
@@ -4,7 +4,7 @@ import json
 # from lxml import etree
 import lxml.html
 class get_music_all_url():
-    def __init__(self,music_name,music_list_id=0):
+    def __init__(self,music_name="",music_list_id=0):
         self.music_name = music_name
         self.music_list_id=music_list_id
         self.init_get_json()#获得 self.json_get 一个大大的字典
@@ -46,7 +46,7 @@ class get_music_all_url():
         author=self.json_get["data"]["song"]["list"][self.music_list_id]["singer"][0]["name"]
         poster= ""
         print({"src":xx,"name":name,"author":author,"poster":poster   })
-        return json.dumps({"src":xx,"name":name,"author":author,"poster":poster   })
+        return json.dumps({"src":xx,"name":name,"author":author,"poster":poster,"list":self.json_get["data"]["song"]["list"] })
 
     def init_get_json(self):
         headers = {
@@ -64,9 +64,30 @@ class get_music_all_url():
         print(json.loads(respones)["data"]["song"]["list"][0]["title"])#歌曲名字
         self.json_get = json.loads(respones)
         return self.json_get
-
+#返回的是 搜索的结果列表 0-19
     def get_search_list(self):#返回一个列表 用于列出view 小程序的view
         return self.json_get["data"]["song"]["list"]
-# if __name__ == '__main__':
-#     p=get_music_all_url(music_name="遥远的她")
-#     p.get_music_song()
+#返回的是 一个列表包含hot image [0]["picUrl"] 0-4
+    def get_hot_image(self):
+        url = "https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&_=1546562309090"
+        headers = {
+            # "referer": "https://i.y.qq.com/v8/playsong.html?ADTAG=newyqq.song&songmid=003sYHuC3aBd4r"
+            "user-agent": "Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+            # "upgrade-insecure-requests": "1",
+            # "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            # "accept-encoding": "gzip, deflate, br",
+            # "accept-language": "zh-CN,zh;q=0.9",
+        }
+        response = requests.get(headers=headers,url=url).text
+        # response = requests.get(headers=headers,url=url).content
+        # etree = lxml.html.etree
+        # selector = etree.HTML(response)
+        # xx = selector.xpath("//body")
+        # with open("test.html","wb") as f:
+        #     f.write(response)
+        # print(json.loads(response)["data"]["slider"][0]["picUrl"])
+        return json.loads(response)["data"]["slider"] #返回一个字典 loads 是加载json字符变成字典
+if __name__ == '__main__':
+    p=get_music_all_url(music_name="遥远的她")
+    # p.get_music_song()
+    p.get_hot_image()
