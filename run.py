e6db815055aa4f992f00213c9890af7db9cf98bc
Xuenew <18188108851@163.com>
2019-01-04 20:17:37 +0800

    2019.1.4 night do

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from spider_music_get import get_music_all_url
-
+import json
 app = Flask(__name__)
-
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -17,13 +15,22 @@ def back_song_list(name):
     name=name
     p=get_music_all_url(music_name=name)
     return p.get_music_song()
-@app.route("/index_hot_image",methods=['GET'])
+@app.route("/index_hot_image")
 def back_index_hot_image():
     """
     返回 hot image 滑动的图片list
     :return:
     """
     p=get_music_all_url()
-    return p.get_hot_image()
+    return json.dumps(p.get_hot_image())
+    # return "HI"
+@app.route("/get_song/<mid>",methods=['GET'])
+def back_song_musicl(mid):
+    mid = mid[4:]
+    print(mid,"*******************",type(mid))
+    # 测试用的 mid ：004AXFnZ33pU2z
+    p=get_music_all_url()
+    print(p.get_music_src(mid))
+    return p.get_music_src(mid)
 if __name__ == '__main__':
     app.run()
--- spider_music_get.py
+++ spider_music_get.py
@@ -4,14 +4,27 @@ import json
 # from lxml import etree
 import lxml.html
 class get_music_all_url():
+    """
+    首页 网页版 返回数据链接
+    https://u.y.qq.com/cgi-bin/musicu.fcg?-=recom27554676684513946&g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0&data=%7B%22comm%22%3A%7B%22ct%22%3A24%7D%2C%22category%22%3A%7B%22method%22%3A%22get_hot_category%22%2C%22param%22%3A%7B%22qq%22%3A%22%22%7D%2C%22module%22%3A%22music.web_category_svr%22%7D%2C%22recomPlaylist%22%3A%7B%22method%22%3A%22get_hot_recommend%22%2C%22param%22%3A%7B%22async%22%3A1%2C%22cmd%22%3A2%7D%2C%22module%22%3A%22playlist.HotRecommendServer%22%7D%2C%22playlist%22%3A%7B%22method%22%3A%22get_playlist_by_category%22%2C%22param%22%3A%7B%22id%22%3A8%2C%22curPage%22%3A1%2C%22size%22%3A40%2C%22order%22%3A5%2C%22titleid%22%3A8%7D%2C%22module%22%3A%22playlist.PlayListPlazaServer%22%7D%2C%22new_song%22%3A%7B%22module%22%3A%22QQMusic.MusichallServer%22%2C%22method%22%3A%22GetNewSong%22%2C%22param%22%3A%7B%22type%22%3A0%7D%7D%2C%22new_album%22%3A%7B%22module%22%3A%22music.web_album_library%22%2C%22method%22%3A%22get_album_by_tags%22%2C%22param%22%3A%7B%22area%22%3A1%2C%22company%22%3A-1%2C%22genre%22%3A-1%2C%22type%22%3A-1%2C%22year%22%3A-1%2C%22sort%22%3A2%2C%22get_tags%22%3A1%2C%22sin%22%3A0%2C%22num%22%3A40%2C%22click_albumid%22%3A0%7D%7D%2C%22toplist%22%3A%7B%22module%22%3A%22music.web_toplist_svr%22%2C%22method%22%3A%22get_toplist_index%22%2C%22param%22%3A%7B%7D%7D%2C%22focus%22%3A%7B%22module%22%3A%22QQMusic.MusichallServer%22%2C%22method%22%3A%22GetFocus%22%2C%22param%22%3A%7B%7D%7D%7D
+    并不清楚是否每天更改
+
+    热点词汇 链接
+    https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg?g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0
+
+    新碟首发 链接 albummid = 首页 网页版返回的数据
+    https://c.y.qq.com/v8/fcg-bin/fcg_v8_album_info_cp.fcg?albummid=002zt7Im2HVifc&g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0
+    上面这个链接获得 一个数据 里面有可以的 mid  通过类得到
+    """
     def __init__(self,music_name="",music_list_id=0):
         self.music_name = music_name
         self.music_list_id=music_list_id
-        self.init_get_json()#获得 self.json_get 一个大大的字典
-        # self.music_song_id=self.json_get["data"]["song"]["list"][self.music_list_id]["singer"][0]["mid"]
-        self.music_song_id=self.json_get["data"]["song"]["list"][self.music_list_id]["mid"]#手机版的 song_id和歌词页一样
-        self.music_mv_id=self.json_get["data"]["song"]["list"][self.music_list_id]["mv"]["vid"]
-        self.music_lyrics_id=self.json_get["data"]["song"]["list"][self.music_list_id]["mid"]#歌词暂时先获取ID 在写函数
+        if music_name !="":
+            self.init_get_json()#获得 self.json_get 一个大大的字典
+            # self.music_song_id=self.json_get["data"]["song"]["list"][self.music_list_id]["singer"][0]["mid"]
+            self.music_song_id=self.json_get["data"]["song"]["list"][self.music_list_id]["mid"]#手机版的 song_id和歌词页一样
+            self.music_mv_id=self.json_get["data"]["song"]["list"][self.music_list_id]["mv"]["vid"]
+            self.music_lyrics_id=self.json_get["data"]["song"]["list"][self.music_list_id]["mid"]#歌词暂时先获取ID 在写函数
 
     # @classmethod
     def get_music_song(self,):
@@ -47,7 +60,26 @@ class get_music_all_url():
         poster= ""
         print({"src":xx,"name":name,"author":author,"poster":poster   })
         return json.dumps({"src":xx,"name":name,"author":author,"poster":poster,"list":self.json_get["data"]["song"]["list"] })
-
+    @staticmethod
+    def get_music_src(song_id):
+        header = {
+            # "referer": "https://i.y.qq.com/v8/playsong.html?ADTAG=newyqq.song&songmid=003sYHuC3aBd4r"
+            "user-agent": "Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+            # "upgrade-insecure-requests": "1",
+            # "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            # "accept-encoding": "gzip, deflate, br",
+            # "accept-language": "zh-CN,zh;q=0.9",
+        }
+        url = "https://i.y.qq.com/v8/playsong.html?ADTAG=newyqq.song&songmid={}#webchat_redirect".format(song_id)
+        respones = requests.get(url=url, headers=header, ).text
+        etree = lxml.html.etree
+        selector = etree.HTML(respones)
+        xx = selector.xpath("//audio[@id='h5audio_media']/@src")
+        pic = selector.xpath("//body/article / section[1] / div / img/@src")
+        # / html / body / article / section[2] / div / img
+        print(pic)
+        print(xx)
+        return json.dumps({"src":xx})
     def init_get_json(self):
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36"
@@ -66,7 +98,7 @@ class get_music_all_url():
         return self.json_get
 #返回的是 搜索的结果列表 0-19
     def get_search_list(self):#返回一个列表 用于列出view 小程序的view
-        return self.json_get["data"]["song"]["list"]
+        return json.dumps(self.json_get["data"]["song"]["list"])
 #返回的是 一个列表包含hot image [0]["picUrl"] 0-4
     def get_hot_image(self):
         url = "https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&_=1546562309090"
@@ -90,4 +122,5 @@ class get_music_all_url():
 if __name__ == '__main__':
     p=get_music_all_url(music_name="遥远的她")
     # p.get_music_song()
-    p.get_hot_image()
+    # p.get_hot_image()
+    p.get_music_src("004X9VIj0TZ62M")
